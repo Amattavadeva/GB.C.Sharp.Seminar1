c5c7aed2afd8725998a1e90da96756529bef910c
Alex QAsper <semidog@gmail.com>
2022-06-09 22:00:23 +0300

    complete tasts from 1 seminar

--- Seminar1.cs
+++ Seminar1.cs
@@ -1,16 +1,56 @@
 ﻿/* Задача 2: Напишите программу, которая на вход
 принимает два числа и выдаёт, какое число большее, а
-какое меньшее. */
+какое меньшее.
 
-/* int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 
 if (a > b)
 {
-    Console.WriteLine(a);
+    Console.WriteLine("max = " + a);
 }
 else
 {
     Console.WriteLine(b);
-} */
+} 
+*/
 
+
+/*Задача 4: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b & a > c){
+    Console.WriteLine(a);
+}
+else if (b > a & b > c){
+    Console.WriteLine(b);
+}
+else Console.WriteLine(c);
+*/
+
+/* Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным 
+
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0){
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
+*/
+
+/* Задача 8: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N. */
+
+int a = int.Parse(Console.ReadLine());
+
+for (int x = 1; x <= a; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
